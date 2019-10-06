57542dc59d214399dfeb4f3f8c5cae84e86cc3dc
avalotaire <alexis.valotaire@gmail.com>
2019-10-06 19:43:40 -0400

    Code is now done

--- code/app.py
+++ code/app.py
@@ -1,80 +1,76 @@
-from flask import Flask, render_template, request, jsonify, json, flash
+from flask import Flask, render_template, request
 import datetime
 from ssl import SSLContext, PROTOCOL_SSLv23
 from py_scripts.find_astro_sign import find_sign
-
 import re
 
 app = Flask(__name__)
+
+# Horoscope dictionnary
+horoscope_dict =	{
+    "Sagittaire": "Vous serez riche.",
+    "Capricorne": "Vous ne serez pas riche.",
+    "Verseau": "Vous trouverez l'amour.",
+    "Poissons": "Vous ne trouverez pas l'amour.",
+    "Bélier": "Vous serez chanceux aujourd'hui.",
+    "Taureau": "Vous ne serez pas chanceux aujourd'hui.",
+    "Gémeaux": "Vous serez riche.",
+    "Cancer": "Vous ne serez pas riche.",
+    "Lion": "Vous trouverez l'amour.",
+    "Vierge": "Vous ne trouverez pas l'amour.",
+    "Balance": "Vous serez chanceux aujourd'hui.",
+    "Scorpion": "Vous ne serez pas chanceux aujourd'hui."
+}
 
 # If GET, display normal page
 @app.route('/', methods=['GET'])
 @app.route('/', methods=['POST'])
 def home():
-    if request.method == 'POST':
-        first_name = request.form['first_name_input']
-        last_name = request.form['last_name_input']
-        birthday_date = request.form['birthday_date']
-
-
     return render_template('accueil.html', page_title='Horoscope', app_title='Horoscope')
 
-# @app.route('/horoscope')
-# def add_numbers():
-#     a = request.args.get('a', 0, type=int)
-#     b = request.args.get('b', 0, type=int)
-#     return jsonify(result=a + b)
 
 @app.route('/horoscope', methods=['POST'])
 def display_horoscope():
+    # Init var with post request
     last_name = request.form['last_name_input']
     first_name = request.form['first_name_input']
     birthday_date = request.form['birthday_date']
-    status = ''
 
+    # Init status var
+    status = 'Les paramètres suivants sont manquant : '
+    status_arg = ''
+
+    # Check if date is correctly formatted
     check_date = re.match('(\d{2})[/.-](\d{2})[/.-](\d{4})$',birthday_date)
 
     # Validate input
     if last_name == '':
-        status += 'Il manque le nom de famille \n'
+        status_arg += ' | nom de famille | '
 
     if first_name == '':
-        status += 'Il manque le prénom \n'
+        status_arg += ' | prénom | '
 
     if birthday_date == '':
-        status += 'Il manque la date de naissance \n'
+        status_arg += ' | date de naissance | '
 
+    # If missing input
+    if status_arg:
+        status += status_arg
+        return render_template('input_error.html', status=status)
+
+    # If date incorrectly formatted
     if not check_date:
-        status += 'Veuillez entrer une date sous la forme mm/dd/yyyy'
+        status = 'Veuillez entrer une date sous la forme mm/dd/yyyy'
+        return render_template('input_error.html', status=status)
 
-    print(status)
-    if status:
-        return render_template('horoscope_display.html', status=status)
-
+    # Transfert birthdate into datetime
     date = datetime.datetime.strptime(birthday_date, "%m/%d/%Y")
 
-    print(last_name)
-    print(first_name)
-    print(date)
-
-    #
+    # Call the script to find the astrology sign using the birthdate
     astro_sign = find_sign(date.month, date.day)
-    print(astro_sign)
 
-    return render_template('horoscope_splay.html', astro_sign=astro_sign)
-    # return jsonify(astro_sign)
-    # last_name = request.args.get('last_name_input', 0, type=str)
-    # first_name = request.args.get('first_name_input', 0, type=str)
-    # birthday_date = request.args.get('birthday_date', 0, type=str)
-    # print(last_name)
-    # print(first_name)
-    # print(birthday_date)
-    # date = datetime.datetime.strptime(birthday_date, "%m/%d/%Y")
-    #
-    # astro_sign = find_sign(date.month, date.day)
-    # print(astro_sign)
-    #
-    # return jsonify(result=astro_sign, result2=last_name)
+    return render_template('horoscope_display.html', astro_sign=astro_sign, horoscope_dict=horoscope_dict[astro_sign],
+                           first_name=first_name, last_name=last_name)
 
 # If page not found, display this html
 @app.errorhandler(404)
@@ -83,7 +79,8 @@ def page_not_found(e):
 
 # When called, run the server
 if __name__ == '__main__':
-    context = SSLContext(PROTOCOL_SSLv23)
-    context.load_cert_chain('./SSL.crt', './SSL.key')
+    # To run with https
+    # context = SSLContext(PROTOCOL_SSLv23)
+    # context.load_cert_chain('./SSL.crt', './SSL.key')
+    # Start the server
     app.run(host='127.0.0.1', debug=True, port=8080)
-    # app.run()
--- code/static/js/custom_script.js
+++ code/static/js/custom_script.js
@@ -1,16 +1,7 @@
-// Used for datepicker
+// Used for datepicker birthdate input
 $( "#birthday_date_input" ).datepicker();
 
-// Used to display horoscope using AJAX
-// $('a#request_horoscope').bind('click', function() {
-//     $.getJSON('/horoscope', {
-//         a: $('input[name="a"]').val(),
-//         b: $('input[name="b"]').val()
-//     }, function(data) {
-//         $("#result").text(data.result);
-//     });
-//     return false;
-// });
+// Ajax function to process input
 $(document).ready(function(){
     $("#request_horoscope").click(function(){
         last_name_input = $('#last_name_input').val();
@@ -25,8 +16,8 @@ $(document).ready(function(){
                 'birthday_date': birthday_date_input
             },
             success: function(result){
-				// $('#horoscope_display').text(result)
-                $(this).html(result)
+				$('#horoscope_display').html(result)
+                // console.log(result)
 			},
 			error: function(error){
 				console.log(error);
@@ -35,17 +26,11 @@ $(document).ready(function(){
     });
 });
 
-// $(function() {
-//     $('#request_horoscope').bind('click', function() {
-//         $.getJSON('/horoscope', {
-//             last_name_input: $('input[name="last_name_input"]').val(),
-//             first_name_input: $('input[name="first_name_input"]').val(),
-//             birthday_date: $('input[name="birthday_date"]').val()
-//         }, function(data) {
-//             $("#astro_sign_id").text(data.result);
-//             console.log(data.result);
-//             console.log(data.result2);
-//         });
-//         return false;
-//     });
-// });
+$(document).ready(function () {
+    $("#reset_input").click(function(){
+        $('#last_name_input').val('');
+        $('#first_name_input').val('');
+        $('#birthday_date_input').val('');
+    })
+
+})
